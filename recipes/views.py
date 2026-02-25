9796b23cdd6686d2ddfce38d7b7070e64062b8ba
iohanjheremy <iohanjheremy11@gmail.com>
2026-02-25 14:38:47 -0400

    Usando get_list_or_404 para atualizar o titulo da pagina

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -14,12 +14,17 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id = category_id,
-        is_published = True,
-    ).order_by('-id')
+    
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id = category_id,
+            is_published = True,
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'titulo': f'{recipes[0].category.name} - Category | '
     })
 
 def recipe(request, id):
