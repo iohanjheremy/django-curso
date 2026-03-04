90d9fd6fc0df6fd519c6b41f72dfad291646eaf8
iohanjheremy <iohanjheremy11@gmail.com>
2026-03-04 15:15:36 -0400

    Iniciando com testes no Django com Pytest, Pytest-Django e Unittest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        var = '1231231'
+        print('ola mundo')
+        assert 1 == 1, 'coisas'
